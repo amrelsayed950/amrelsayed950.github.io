dc73582408420eeee0cfd58ebe47e9328935b8ee
AmrDroid <amrmostafa1995@gmail.com>
2023-03-15 19:31:32 -0500

    bugs fixed

--- lab12/fifteen.js
+++ lab12/fifteen.js
@@ -1,21 +1,21 @@
-(function(){
-    "use strict";
+(function () {
     //object for empty box
-    var emptyTop=300;
-    var emptyLeft=300;
+    var emptyTop = 300;
+    var emptyLeft = 300;
     var divsStart; //to store the initial information of the divs
-    $(document).ready(function(){
+    $(document).ready(function () {
+        "use strict";
         init();
 
-        divsStart=divsAtStart();
+        divsStart = divsAtStart();
         //if any box is clicked, then it is checked whether it is moveable or not,
         //if so, then its position is changed with empty box.
         //after each move, position is compared to find out whether game is completed or not.
-        $(".puzzlepiece").click(function(){
+        $(".puzzlepiece").click(function () {
             var current = $(this).position();
-            if(isMoveable(current)){
+            if (isMoveable(current)) {
                 moveSquare($(this));
-                if(checkIfComplete()){
+                if (checkIfComplete()) {
                     alert("Yes you did it...")
                 }
             }
@@ -33,7 +33,7 @@
         }
 
         //if shuffle button is clicked, then box are suffled
-        $("#shufflebutton").click(function() {
+        $("#shufflebutton").click(function () {
             //shuffles the boxes
             //order of O(nm) where n->number of times we run outer loop
             //and m->number of elements in box (in this case 16)
@@ -68,38 +68,38 @@
     });
     //initial position of the divs which is used to compare with the divs at given
     //time to determine whether the game is finished or not.
-    var divsAtStart=function(){
-        var divPosition=[];
-        var divs=$(".puzzlepiece");
-        for(var i=0;i<divs.length;i++){
-            divPosition[i]={
-                x:$(divs[i]).position.left,
-                y:$(divs[i]).position.top
+    var divsAtStart = function () {
+        var divPosition = [];
+        var divs = $(".puzzlepiece");
+        for (var i = 0; i < divs.length; i++) {
+            divPosition[i] = {
+                x: $(divs[i]).position.left,
+                y: $(divs[i]).position.top
             }
         }
         return divPosition;
     }
 
     //gives the divs after each movement
-    var divsNow=function(){
-        var divPosition=[];
-        var divs=$(".puzzlepiece");
-        for(var i=0;i<divs.length;i++){
-            divPosition[i]={
-                x:$(divs[i]).position.left,
-                y:$(divs[i]).position.top
+    var divsNow = function () {
+        var divPosition = [];
+        var divs = $(".puzzlepiece");
+        for (var i = 0; i < divs.length; i++) {
+            divPosition[i] = {
+                x: $(divs[i]).position.left,
+                y: $(divs[i]).position.top
             }
         }
         return divPosition;
     }
 
     //to find out whether the matching is finished
-    var checkIfComplete=function(){
-        var divNow=divsNow();
+    var checkIfComplete = function () {
+        var divNow = divsNow();
         var status = true;
-        for(var i=0;i<divNow.length;i++){
-            if(Math.floor(divNow[i].left) !== Math.floor(divsStart[i].left) ||
-                Math.floor(divNow[i].top) !== Math.floor(divsStart[i].top)){
+        for (var i = 0; i < divNow.length; i++) {
+            if (Math.floor(divNow[i].left) !== Math.floor(divsStart[i].left) ||
+                Math.floor(divNow[i].top) !== Math.floor(divsStart[i].top)) {
                 status = false;
             }
         }
@@ -108,26 +108,26 @@
 
     //determines whether the input box is moveable or not on the basis of
     //position of empty box
-    var isMoveable=function(current){
-        if(( emptyTop=== current.top &&
+    var isMoveable = function (current) {
+        if ((emptyTop === current.top &&
             (emptyLeft === current.left + 100 ||
-            emptyLeft === current.left - 100)) ||
+                emptyLeft === current.left - 100)) ||
             (emptyLeft === current.left &&
-            (emptyTop === current.top + 100 ||
-            emptyTop === current.top - 100))){
+                (emptyTop === current.top + 100 ||
+                    emptyTop === current.top - 100))) {
             return true;
         }
         return false;
     }
-    var init = function() {
+    var init = function () {
         var divs = $("#puzzlearea div");
         // initialize each piece
-        for (var i=0; i< divs.length; i++) {
+        for (var i = 0; i < divs.length; i++) {
             var div = divs[i];
 
             // calculate x and y for this piece
-            var x = ((i % 4) * 100) ;
-            var y = (Math.floor(i / 4) * 100) ;
+            var x = ((i % 4) * 100);
+            var y = (Math.floor(i / 4) * 100);
 
             // set basic style and background
             div.className = "puzzlepiece";
